ce97de0a0961e3fdb36f8cc11bb6ca541f73f18e
David Haz <davidhzdev@gmail.com>
2025-08-24 17:56:17 +0300

    Reduce MAX_STEPS from 20 to 15

--- src/components/landing/PlasmaWave/PlasmaWaveV2.jsx
+++ src/components/landing/PlasmaWave/PlasmaWaveV2.jsx
@@ -31,7 +31,7 @@ const float lt   = 0.3;
 const float pi   = 3.141592653589793;
 const float pi2  = pi * 2.0;
 const float pi_2 = pi * 0.5;
-#define MAX_STEPS 20
+#define MAX_STEPS 15
 #define A(v) mat2(cos(m.v + radians(vec4(0.0,-90.0,90.0,0.0))))
 
 void mainImage(out vec4 C, in vec2 U) {
