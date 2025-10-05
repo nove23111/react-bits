1b7e04e98be52249f06c17898ad6c4f875cc489c
nove23111 <145318041+nove23111@users.noreply.github.com>
2025-10-06 02:12:44 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import React from 'react';
 import DotGrid from './DotGrid';
 
-<div style={{ width: '100%', height: '600px', position: 'relative' }}>
-  <DotGrid
-    dotSize={10}
-    gap={15}
-    baseColor="#5227FF"
-    activeColor="#5227FF"
-    proximity={120}
-    shockRadius={250}
-    shockStrength={5}
-    resistance={750}
-    returnDuration={1.5}
-  />
-</div>
+export default function App() {
+  return (
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <DotGrid
+        dotSize={10}
+        gap={15}
+        baseColor="#5227FF"
+        activeColor="#5227FF"
+        proximity={120}
+        shockRadius={250}
+        shockStrength={5}
+        resistance={750}
+        returnDuration={1.5}
+      />
+    </div>
+  );
+}
